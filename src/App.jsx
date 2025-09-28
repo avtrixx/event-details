b1ea621be2cc3f1e8e21f8994656db7d7654b2db
avtrixx <avtrixx@gmail.com>
2025-09-28 19:46:33 +0530

    Changes to have mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -156,7 +156,15 @@ function App() {
                 placeholder="Type your question..."
                 onKeyDown={(e) => e.key === 'Enter' && handleSend()}
               />
-              <button onClick={handleSend}>Send</button>
+              {isMobile ? (
+                <button onClick={handleSend} aria-label="Send">
+                  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M3 20L21 12L3 4V10L15 12L3 14V20Z" fill="#fff"/>
+                  </svg>
+                </button>
+              ) : (
+                <button onClick={handleSend}>Send</button>
+              )}
             </div>
           </div>
         ) : (
